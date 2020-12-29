999882deddbae3951180a9f5a8922a30aa9aa816
nhannt201 <trungnhan21.12@gmail.com>
2020-12-29 16:03:03 +0700

    add code test

---
+++ back_end/index.php
@@ -0,0 +1,26 @@
+
+<?php
+
+include('simple_html_dom.php');
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://nha.chotot.com/toan-quoc/thue-bat-dong-san?q=binh%20thanh&price=0-5500000');
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($curl);
+curl_close($curl);
+//echo $result;
+
+$domResult = new simple_html_dom();
+$domResult->load($result);
+
+foreach($domResult->find('li[class=wrapperAdItem___2woJ1]') as $list) //Lay duoc list
+foreach($list->find('a') as $element)
+       echo $element->href . '<br>';
+
+/**
+Dùng thư viện HTML_DOC để lấy một phần trong trang web chỉ định với các thông tin có sẵn như attribute, div, etc.
+**/
+?>
+
+

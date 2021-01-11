033742f68a568f1b9cd2488893b24f3764ec8752
nhannt201 <trungnhan21.12@gmail.com>
2021-01-11 17:10:51 +0700

    add

--- index.php
+++ index.php
@@ -86,7 +86,7 @@
               </svg>
             </div>
             <input id="search" name="q" type="text" placeholder="Bạn muốn tìm phòng trọ ở đâu?" >
-			  <div class="svg-wrapper">
+			  <div class="svg-wrapper" onClick="getLocation();">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.343-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>
             </div>
           </div>
@@ -136,5 +136,19 @@
 	
   } );
    </script>-->
+   <script>
+var x = document.getElementById("search");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.value = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.value = position.coords.latitude + " " + position.coords.longitude;
+}
+</script>
   </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
